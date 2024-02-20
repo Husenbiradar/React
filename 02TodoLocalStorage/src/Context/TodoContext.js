4d830003cb069b1eb06618e133c468ccae122679
Husenbiradar <147536277+Husenbiradar@users.noreply.github.com>
2024-02-20 13:01:23 +0000

    sd

--- 02TodoLocalStorage/src/Context/TodoContext.js
+++ 02TodoLocalStorage/src/Context/TodoContext.js
@@ -1,11 +1,17 @@
 import {createContext, useContext} from 'react'
 
 export const TodoContext = createContext({
-    todos : {
-        id : 1,
-        tood : 'Todo msg',
-        completed : false ,
-    }
+    todos : [
+        {
+            id : 1,
+            tood : 'Todo msg',
+            completed : false ,
+        }
+    ],
+    addTodo : (todo)=> {},
+    updateTodo : (id, todo)=>{},
+    deleteTodo : (id)=>{}, 
+    toggleComplete : (id)=> {}
 })
 
 export const useTodo= ()=>{
