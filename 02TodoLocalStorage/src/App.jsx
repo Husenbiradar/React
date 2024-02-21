9c87c1ef0f80ac123314078a630d582e2349b335
Husenbiradar <147536277+Husenbiradar@users.noreply.github.com>
2024-02-21 06:15:37 +0000

    ere

--- 02TodoLocalStorage/src/App.jsx
+++ 02TodoLocalStorage/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {TodoProvider} from './Context'
@@ -8,11 +8,35 @@ function App() {
   const [todos, setTodos] = useState([])
 
   const addTodo = (todo)=>{
-    setTodos((prev)=> [{id : Date.now()},...prev])
+    setTodos((prev)=> [{id : Date.now(), ...todo}, ...prev])
   }
 
+  const updateTodo = (id, todo)=>{
+    setTodos((prev)=> prev.map((prevTodo)=> (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deletTodo = (id)=>{
+    setTodos((prev)=> prev.filter((todo)=> todo.id !== id))
+  }
+
+  const toggleComplete= ()=>{
+    setTodos((prev)=> prev.map((prevTodo)=> prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  }
+
+  useEffect(()=>{
+    const todo =JSON.parse(localStorage.getItem('todos'))
+
+    if (todos && todos.lenght > 0) {
+      setTodos(todos)
+    }
+  },[])
+
+useEffect(()=>{
+  localStorage.setItem
+},[])
+
   return (
- <TodoProvider value={{todos, addTodo, UpdateTodo, delettodo, toggleComplete}} >
+ <TodoProvider value={{todos, addTodo, updateTodo, deletTodo, toggleComplete}} >
   <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
