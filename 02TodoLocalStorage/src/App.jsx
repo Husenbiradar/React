dbc85212733b1c364c6861906c6498cde22813a9
Husenbiradar <147536277+Husenbiradar@users.noreply.github.com>
2024-03-25 10:24:23 +0000

    Tailwind Css Practice

--- 02TodoLocalStorage/src/App.jsx
+++ 02TodoLocalStorage/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   const updateTodo = (id, todo)=>{
     setTodos((prev)=> prev.map((prevTodo)=> (prevTodo.id === id ? todo : prevTodo)))
-  }
+  }                                  
 
   const deleteTodo = (id)=>{
     setTodos((prev)=> prev.filter((todo)=> todo.id !== id))
